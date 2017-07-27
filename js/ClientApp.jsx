1ec0ab546e8196205aaf498008b67286299624ab
Annie Huddleston <ahuddleston@remedyhealthmedia.com>
2017-07-27 17:05:50 -0400

    convert initial stuff to jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const ce = React.createElement;
-
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  return (
+    <div>
+      <h1 style={ {color: props.color} }>{props.title}</h1>
+    </div>
+  )
 };
 
 const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
-  );
+  return <div id="my-first-component">
+    <MyTitle title='Masterpiece Theater' color='YellowGreen' />
+    <MyTitle title='Jane the Virgin' color='GreenYellow' />
+    <MyTitle title='Pride and Prejudice' color='lime' />
+  </div>
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<MyFirstComponent />, document.getElementById('app'));
