a1010546daacb998691c4d045c1259382c9776ed
Annie Huddleston <ahuddleston@remedyhealthmedia.com>
2017-07-28 09:12:28 -0400

    now in jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const MyTitle = function(props) {
-  return (
-    <div>
-      <h1 style={ {color: props.color} }>{props.title}</h1>
+const App = () => (
+    <div className='app'>
+      <div className='landing'>
+        <h1>Video by React</h1>
+        <input type='text' placeholder='Search' />
+        <a>or Browse All</a>
+      </div>
     </div>
   )
-};
 
-const MyFirstComponent = function() {
-  return <div id="my-first-component">
-    <MyTitle title='Masterpiece Theater' color='YellowGreen' />
-    <MyTitle title='Jane the Virgin' color='GreenYellow' />
-    <MyTitle title='Pride and Prejudice' color='lime' />
-  </div>
-};
-
-render(<MyFirstComponent />, document.getElementById('app'));
+render(<App />, document.getElementById('app'))
