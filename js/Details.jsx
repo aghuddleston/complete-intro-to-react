40b92a42d6588388a65c889c10037f49ea02fd5a
Annie Huddleston <ahuddleston@remedyhealthmedia.com>
2017-07-31 10:59:16 -0400

    preload now in App.jsx and rough of Details component

---
+++ js/Details.jsx
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react'
+
+const Details = (props) => (
+  <div className='details'>
+    <pre><code>{JSON.stringify(props, null, 2)}</code></pre>
+
+  </div>
+)
+
+export default Details
